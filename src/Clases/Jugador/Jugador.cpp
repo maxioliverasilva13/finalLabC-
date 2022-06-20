e078386641f6185285f5bd65a13ea150b6088108
Maximiliano <angelotunado02@gmail.com>
2022-06-20 13:51:54 -0300

    fix finalizarPartida

--- src/Clases/Jugador/Jugador.cpp
+++ src/Clases/Jugador/Jugador.cpp
@@ -105,7 +105,7 @@ void Jugador::continuarPartida(int idPart)
     PartidaIndividual *part = (PartidaIndividual *)this->partidas->find(partKey);
     if (part)
     {
- 
+        part->continuarPartida();
     }
 } 
 
